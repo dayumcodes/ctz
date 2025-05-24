450ad40b3810e8acf86a07b864bdb337877b7a05
dayumcodes <dayumcodes@gmail.com>
2025-05-25 01:55:47 +0530

    2nd

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
 import type {NextConfig} from 'next';
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -20,4 +24,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
